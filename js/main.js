502781f7c4dd19898b72d579c91f4f778cd4af79
emanuele5180 <macina5180>
2020-07-07 15:23:33 +0200

    add and remove

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-  $('nav-main-dropdown').mouseEnter(function(){
-    $(this).addClass('active');
+  $('.nav-main-dropdown').mouseenter (function(){
+    $('.dropdown-menu').addClass('active');
+    console.log('.dropdown-menu');
+  })
+  $('.nav-main-dropdown').mouseleave (function(){
+    $('.dropdown-menu').removeClass('active');
+    console.log('.dropdown-menu');
   })
 })
