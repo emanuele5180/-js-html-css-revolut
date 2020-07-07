0d2850511ad9c0abdd82b8363fb0efb5ee7cc7a2
emanuele5180 <macina5180>
2020-07-07 23:53:49 +0200

    jumbo

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-  $('.nav-main-dropdown').mouseenter (function(){
+  $('.nav-main-dropdown a').mouseenter (function(){
     $('.dropdown-menu').addClass('active');
     console.log('.dropdown-menu');
   })
-  $('.nav-main-dropdown').mouseleave (function(){
+  $('.nav-main-dropdown a').mouseleave (function(){
     $('.dropdown-menu').removeClass('active');
     console.log('.dropdown-menu');
   })
