540a83a6d94844a1bc00bf13b024ad14ef09475a
emanuele5180 <macina5180>
2020-07-08 13:30:42 +0200

    dropdown hidden

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-  $('.nav-main-dropdown a').mouseenter (function(){
-    $('.dropdown-menu').addClass('active');
-    console.log('.dropdown-menu');
+  $('.nav-main-dropdown').mouseenter (function(){
+    $(this).children('.hidden').addClass('active');
+    //console.log('li dropdown-menu');
   })
-  $('.nav-main-dropdown a').mouseleave (function(){
-    $('.dropdown-menu').removeClass('active');
-    console.log('.dropdown-menu');
+  $('.nav-main-dropdown').mouseleave (function(){
+    $(this).children('.hidden').removeClass('active');
+    //console.log('.dropdown-menu');
   })
 })
